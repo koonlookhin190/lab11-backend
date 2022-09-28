3019ca8bfc150f5790df4c8135a670cab25de8cb
koonlookhin <krit_tipnuan@cmu.ac.th>
2022-09-28 23:20:59 +0700

    1.4

--- src/main/java/se331/rest/repository/EventRepository.java
+++ src/main/java/se331/rest/repository/EventRepository.java
@@ -11,4 +11,7 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event,Long> {
     List<Event> findAll();
     Page<Event> findByTitle(String title, Pageable pageRequest);
+
+    Page<Event> findByTitleContaining(String title, Pageable pageRequest);
+    
 }
